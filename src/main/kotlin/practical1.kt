3ab07a4f96fc8ea3b13aafe5d3b4087c0839bfe1
om7001 <ommaniya123@gmail.com>
2022-08-21 17:29:12 +0530

    hello

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,4 +1,21 @@
 fun main()
 {
-    println("Hello")
+    var i:Int = 10
+    println("Integer Value: $i")
+    var f:Float = 1.3F
+    println("Float Value: $f")
+    var c:Char = 'H'
+    println("Character Value: $c")
+    var s:String = "harsh"
+    println("String Value: $s")
+    var b:Boolean = false
+    println("Boolean Value: $b")
+    var d:Double = 10.1
+    println("Double Value: $d")
+    var l:Long = 123456789
+    println("Long Value: $l")
+    var sh:Short = -54
+    println("Short Value: $sh")
+    var by:Byte = 123
+    println("Byte Value: $by")
 }
